d39f38d13ee621e64017de40a0ba990a5406f798
roberto.oliveira <roberto.oliveira@virtus.ufcg.edu.br>
2022-07-09 22:45:29 -0300

    Add project structure

---
+++ src/main/java/com/roberto/cbf/CbfApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.cbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberto/cbf/CbfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.cbf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
