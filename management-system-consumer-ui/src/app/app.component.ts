f681f5d99db21d3da834d4b1ba2ba3361a9d0327
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2022-07-24 17:32:07 -0300

    integration - update ui

--- management-system-consumer-ui/src/app/app.component.ts
+++ management-system-consumer-ui/src/app/app.component.ts
@@ -12,6 +12,24 @@ export class AppComponent implements OnInit {
 
   private stompClient: Stomp.Client;
 
+  private teamOne: Team = new Team({id: 1, name: 'teamOne', locale: 'rio'});
+  private teamTwo: Team = new Team({id: 2, name: 'teamTwo', locale: 'rio'});
+  private match: Match = new Match(
+    {
+      id: 1,
+      date: '2022-01-01',
+      country: 'rio',
+      teamOne: this.teamOne,
+      teamTwo: this.teamTwo,
+      teamOneScore: 0,
+      teamTwoScore: 0,
+      status: Status.NOT_STARTED,
+      time: 0
+    }
+  );
+
+  matchList: Match[] = [this.match];
+
   constructor() {
   }
 
@@ -33,5 +51,48 @@ export class AppComponent implements OnInit {
         console.log(result);
       });
     });
+  }
+}
+
+export class Team {
+  id: number;
+  name: string;
+  locale: string;
+
+  constructor(obj: any) {
+    this.id = obj.id ? obj.id : null;
+    this.name = obj.name ? obj.name : null;
+    this.locale = obj.locale ? obj.locale : null;
+  }
+}
+
+export enum Status {
+  NOT_STARTED,
+  STARTED,
+  BREAK,
+  FINISHED
+}
+
+export class Match {
+  id: number;
+  date: any;
+  country: string;
+  teamOne: Team;
+  teamTwo: Team;
+  teamOneScore: number;
+  teamTwoScore: number;
+  status: Status = Status.NOT_STARTED;
+  time: number;
+
+  constructor(obj: any) {
+    this.id = obj.id ? obj.id : null;
+    this.date = obj.date ? obj.date : null;
+    this.country = obj.country ? obj.country : null;
+    this.teamOne = obj.teamOne ? obj.teamOne : null;
+    this.teamTwo = obj.teamTwo ? obj.teamTwo : null;
+    this.teamOneScore = obj.teamOneScore ? obj.teamOneScore : 0;
+    this.teamTwoScore = obj.teamTwoScore ? obj.teamTwoScore : 0;
+    this.status = obj.status ? obj.status : null;
+    this.time = obj.time ? obj.time : null;
   }
 }
